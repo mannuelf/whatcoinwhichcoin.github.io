1cf9a1031bd1414ead37790b6f4f2eb9e7725582
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-08-05 22:55:36 +0200

    make it work

--- sw.js
+++ sw.js
@@ -6,7 +6,11 @@ self.addEventListener('install', function(event) {
         console.log('[Service Worker] Pre-caching static assets')
         cache.addAll([
           'https://mannuelf.github.io/whatcoinwhichcoin.github.io/',
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io//index.html',
+          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/index.html',
+          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/css/app.d45249c1.css',
+          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/app.579be62b.js',
+          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/chunk-vendors.2bb742c7.js',
+          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/app.579be62b.js',
           'https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300',
           'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css',
           'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
