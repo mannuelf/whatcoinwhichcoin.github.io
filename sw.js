9d7e9e2807b42116bcffbab876314d357a8e4fdf
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-08-09 22:24:39 +0200

    update service worker with new dynamic and static variables

--- sw.js
+++ sw.js
@@ -1,47 +1,64 @@
-self.addEventListener('install', function(event) {
+const CACHE_STATIC = 'static-01'
+const CACHE_DYNAMIC = 'dynamic-01'
+
+self.addEventListener('install', function (event) {
   console.log('[Service worker] Installing Service Worker...', event);
   event.waitUntil(
-    caches.open('static')
-      .then(function(cache) {
-        console.log('[Service Worker] Pre-caching static assets')
-        cache.addAll([
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/',
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/index.html',
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/css/app.d45249c1.css',
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/app.579be62b.js',
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/chunk-vendors.2bb742c7.js',
-          'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/app.579be62b.js',
-          'https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300',
-          'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css',
-          'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
-          'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css'
-        ])
-      })
+    caches.open(CACHE_STATIC)
+    .then(function (cache) {
+      console.log('[Service Worker] Pre-caching static assets')
+      cache.addAll([
+        'https://mannuelf.github.io/whatcoinwhichcoin.github.io/',
+        'https://mannuelf.github.io/whatcoinwhichcoin.github.io/index.html',
+        'https://mannuelf.github.io/whatcoinwhichcoin.github.io/css/app.d45249c1.css',
+        'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/app.579be62b.js',
+        'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/chunk-vendors.2bb742c7.js',
+        'https://mannuelf.github.io/whatcoinwhichcoin.github.io/js/app.579be62b.js',
+        'https://fonts.googleapis.com/css?family=Rajdhani:300,600,700|Roboto+Mono:100,300',
+        'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.min.css'
+      ])
+    })
   )
 })
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service worker] Activating Service Worker...', event);
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+    .then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== CACHE_STATIC && key !== CACHE_DYNAMIC) {
+          console.log('[Service Worker] Removing old cache.', key)
+          return caches.delete(key)
+        }
+      })) // waits for all the promises to finish
+    })
+  )
   return self.clients.claim();
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   // console.log('[Service Worker]', event);
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open('dynamic')
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone())
-                  return res
-                })
-            })
-        }
-      })
+    .then(function (response) {
+      if (response) {
+        return response
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC)
+              .then(function (cache) {
+                cache.put(event.request.url, res.clone())
+                return res
+              })
+              .catch(function (err) {
+                console.log('fetch error: ', err)
+              })
+          })
+      }
+    })
   );
 })
